d8dd0be421e04128f84c731aa70f42820ef782d7
Joe Zendle <jzendle@gmail.com>
2013-10-30 14:38:35 -0600

    added pointer chasing to find source port of 'port unreachable' ICMP msg

--- Icmp.c
+++ Icmp.c
@@ -5,31 +5,58 @@
 #include <assert.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 extern int icmpGetTypeFromIpFrame(char *ipFrame) {
 
-  struct iphdr *frame = (struct iphdr *) ipFrame;
+  struct iphdr *iphdr = (struct iphdr *) ipFrame;
+  struct icmphdr *icmp = NULL;
 
-  assert(frame != 0);
+
+  assert(iphdr != 0);
+
   /* do some sanity checks */
 
-  if ( frame->version != 4 && frame->version != 6 ) {
-    errv(0,"ip version mismatch: %d\n", frame->version);
+  if ( iphdr->version != 4 && iphdr->version != 6 ) {
+    errv(0,"ip version mismatch: %d\n", iphdr->version);
     return EXIT_FAILURE;
   }
 
   /* check that this is an icmp msg */
-  if ( frame->protocol !=  IPPROTO_ICMP ) {
-    errv(0,"ip protocol mismatch: %d\n" , frame->protocol );
+  if ( iphdr->protocol !=  IPPROTO_ICMP ) {
+    errv(0,"ip protocol mismatch: %d\n" , iphdr->protocol );
     return EXIT_FAILURE;
   }
 
   /* find offset to first byte of icmp which happens to be the msg type */
-  int msgType = ipFrame[frame->ihl * 4];
+  icmp = (struct icmphdr *)&ipFrame[iphdr->ihl * 4];
 
-  return msgType;
+  /* use char * to allow nice bytewise pointer arithmetic - been a long time */
+  char *tmp = (char *) icmp;
+  int type = icmp->type;
+  int code = icmp->code;
+
+  printf("icmp type: %d code: %d\n",type, code);
+
+  /* lets find the offending port (should be our port) if we have received 'port unreachable' */
+  
+  if ( type == ICMP_DEST_UNREACH && code == ICMP_PORT_UNREACH ) {
+    /* offending ip header should follow icmp header */
+    struct iphdr *echoIphdr =  (struct iphdr *) (tmp + sizeof (struct icmphdr));
+    /* once again, use char * for nice pointer math */
+    tmp = (char *) echoIphdr;
+    /* offending udp message should follow this ip header */
+    struct udphdr *badUdp = ( struct udphdr * ) (tmp + echoIphdr->ihl * 4);
+  
+    printf ("dest port %d\n" , ntohs(badUdp->dest));
+    printf ("source port %d\n" , ntohs(badUdp->source));
+
+  }
+  
+  return type;
 
 }
 
